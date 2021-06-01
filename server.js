97fa466d77148bf616ee72599e54cdd0abd20c52
EkramMohra <mohraekram@gmail.com>
2021-06-01 16:52:29 +0300

    heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 // Mongoose setup
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/peopleDB', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/peopleDB', { useNewUrlParser: true })
 
 app.use('/', api)
 
 const port = 4200
-app.listen(port, function () {
+app.listen(process.env.PORT || port, function () {
     console.log(`Running on port ${port}`)
 })
